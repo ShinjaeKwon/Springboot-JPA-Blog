c07c7de5a05bf399238c9172bcb1c899b100b8a6
JaeKwonShin <sjk6437@gmail.com>
2021-08-02 14:42:44 +0900

    글 삭제 구현

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,9 +1,11 @@
 let index = {
 	init: function() {
-		$("#btn-save").on("click", () => { 
+		$("#btn-save").on("click", () => {
 			this.save();
 		});
-	
+		$("#btn-delete").on("click", () => {
+			this.deleteById();
+		});
 	},
 
 	save: function() {
@@ -11,23 +13,38 @@ let index = {
 			title: $("#title").val(),
 			content: $("#content").val()
 		};
-		
-	
-		$.ajax({ 
+
+
+		$.ajax({
 			type: "POST",
 			url: "/api/board",
-			data: JSON.stringify(data), 
-			contentType: "application/json; charset=UTF-8", 
-			dataType: "json" 
-		}).done(function(resp){ 
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=UTF-8",
+			dataType: "json"
+		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
 			location.href = "/"
-		}).fail(function(error){ 
+		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
-		
+
 	},
-	
+	deleteById: function() {
+		var id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
+			location.href = "/"
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+
+	}
+
 }
 
 index.init();
